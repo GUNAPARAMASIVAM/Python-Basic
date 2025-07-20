24433975adf4230560cfcea240612407b0eb4c43
GUNA P <161914312+GUNAPARAMASIVAM@users.noreply.github.com>
2025-07-20 20:01:40 +0530

    first commit

---
+++ Exception Handling.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=int(input())
+c=input()
+try:
+    print("The sum is:", c/a)
+except:
+    print("An error occurred. Please check your inputs.")
+finally:
+        print("End of program")    
