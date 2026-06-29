765c3d16c9277d0600a8e4481a3bc9b69faad550
GUNA P <161914312+GUNAPARAMASIVAM@users.noreply.github.com>
2026-06-29 18:14:26 +0530

    Add Identy_membership.py

---
+++ Identy_membership.py
@@ -0,0 +1,32 @@
+#Identity Operator
+
+x=['apple','banana']
+y=['apple','banana']
+z=x
+
+print(x)
+print(y)
+print(z)
+print(x ,id(x))
+print(y ,id(y))
+print(z ,id(z))
+print(x is z)
+print(x is y)
+
+#Membership
+
+a="apple"
+print("ae" in a)
+b=["apple","banana","cheery","a"]
+print("apple" in b)
+print("a" in b)
+
+
+data= 123 
+
+print("1" in data)
+
+
+
+
+
